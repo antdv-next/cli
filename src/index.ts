088224d05f8368806c4a4b140f275d7449eef718
lonewolfyx <olddrivero.king@qq.com>
2026-08-18 13:19:21 +0800

    chore(cli): remove unused todo comment from subCommands object
    - Removed commented out 'TODO' line in subCommands object
    - Cleaned up unnecessary comment that was not providing value
    - Maintained all functional command imports and their lazy loading behavior
    - Improved code clarity by removing dead comment code

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { description, name, version } from '../package.json' with { type: 'json'
 const CLI_VERSION = __CLI_VERSION__
 
 const subCommands = {
-    // TODO
     'bug': () => import('./commands/bug.ts').then(r => r.default),
     'bug-cli': () => import('./commands/bug-cli.ts').then(r => r.default),
     'changelog': () => import('./commands/changelog.ts').then(r => r.default),
