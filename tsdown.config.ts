2157e263ad250c9139390cd9e26243ce469c0df3
lonewolfyx <olddrivero.king@qq.com>
2026-08-26 14:48:08 +0800

    build(config): update tsdown configuration for build optimization
    - Disable dts generation to improve build performance
    - Set unbundle option to false for bundle optimization
    - Enable minification for production builds
    - Configure dependencies to include all in bundle
    - Disable code splitting in output options
    - Maintain clean build directory setting

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,8 +5,16 @@ const { version } = JSON.parse(readFileSync('package.json', 'utf-8'))
 
 export default defineConfig({
   clean: true,
-  dts: true,
+  dts: false,
+  unbundle: false,
   define: {
     __CLI_VERSION__: JSON.stringify(version),
+  },
+  minify: true,
+  deps: {
+    onlyBundle: false,
+  },
+  outputOptions: {
+    codeSplitting: false,
   },
 })
